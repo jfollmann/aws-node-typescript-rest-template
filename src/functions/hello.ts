909241dc39d5f0f1e4a8edab0e8e57211b222f43
Jefferson Follmann <jeff.follmann@gmail.com>
2020-11-13 13:13:43 -0300

    Add eslint && Serverless-offline Package

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,9 +1,7 @@
-import { APIGatewayProxyEvent, Context } from "aws-lambda";
-
-export async function get(_event: APIGatewayProxyEvent, _context: Context) {
-    return {
-      headers: { "Content-Type": "application/json" },
-      statusCode: 200,
-      body: JSON.stringify({ data: "hello from lambda" }),
-    };
-  }
+export async function get(event: any) {
+  return {
+    headers: { 'Content-Type': 'application/json' },
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello Lambda Function', event }),
+  };
+}
